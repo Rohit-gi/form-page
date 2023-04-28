9bc266cd903534615a3016fd3a9e6279c4885e9c
Rohit-gi <i.rohit003@gmail.com>
2023-04-28 18:10:16 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState();
+
+  const inputEvent = (event) => {
+    setName(event.target.value);
+  };
+
+  const onSubmit = () => {
+    setFullName(name);
+  };
+
+  return (
+    <>
+      <div>
+        <h1> Hello {fullName}</h1>
+        <input
+          type="text"
+          placeholder="Enter your Name"
+          onChange={inputEvent}
+          value={name}
+        />
+        <button onClick={onSubmit}>Click me👍</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
